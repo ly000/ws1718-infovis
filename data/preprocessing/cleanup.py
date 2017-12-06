c25ea7d25b6b88cee4bbb90b7bf0b8222aac0c67
Long Mathias Yan <yanlo@teublitz.cip.ifi.lmu.de>
2017-12-06 20:41:27 +0100

    added sorted dataset

--- data/preprocessing/cleanup.py
+++ data/preprocessing/cleanup.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 
 def preproc(input, output):
-    unprocessed_data = pd.read_csv(input, index_col="JAHR")
+    unprocessed_data = pd.read_csv(input, index_col="NUMMER")
     #data = pd.DataFrame([np.nan, 2, np.nan, 0], [3, 4, np.nan, 1], [np.nan, np.nan, np.nan, 5], columns)
     data = unprocessed_data.dropna(axis=1, how='all');
+    data = data.sort_index(ascending=True)
     data.to_csv(output)
     print(data.head())
 
