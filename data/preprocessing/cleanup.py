a8fb23ac84abe44e4b94ad769e68401e46ecc7d2
Long Mathias Yan <yanlo@teublitz.cip.ifi.lmu.de>
2017-12-06 20:03:14 +0100

    preproc

---
+++ data/preprocessing/cleanup.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+import numpy as np
+
+def preproc(input, output):
+    unprocessed_data = pd.read_csv(input, index_col="JAHR")
+    #data = pd.DataFrame([np.nan, 2, np.nan, 0], [3, 4, np.nan, 1], [np.nan, np.nan, np.nan, 5], columns)
+    data = unprocessed_data.dropna(axis=1, how='all');
+    data.to_csv(output)
+    print(data.head())
+
+if __name__ == "__main__":
+    inp = sys.argv[1]
+    outp = sys.argv[2]
+    preproc(inp, outp)
