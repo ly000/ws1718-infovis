d84c7e78f722af3644eb911d06389bfd02faeb40
ly000 <long.myan@gmail.com>
2018-01-08 12:29:36 +0100

    added new ds

--- data/preprocessing/cleanup.py
+++ data/preprocessing/cleanup.py
@@ -3,15 +3,19 @@ import pandas as pd
 import numpy as np
 
 def preproc(input, output):
-    unprocessed_data = pd.read_csv(input, decimal=",", index_col="NUMMER")
+    unprocessed_data = pd.read_csv(input, decimal=",")
     #data = pd.DataFrame([np.nan, 2, np.nan, 0], [3, 4, np.nan, 1], [np.nan, np.nan, np.nan, 5], columns)
     data = unprocessed_data.dropna(axis=1, how='all');
-    data = data[data['INDIKATOR_AUSPRAEGUNG'] != 'deutsch'];
-    data = data.sort_index(ascending=True);
-    data['INDIKATOR_WERT'].astype(float);
-    #data = data.groupby('NUMMER').mean();
+    data['JAHR'].astype(int)
+    data = data[data['JAHR'] >= 2007]
+    data = data[data['JAHR'] <= 2014]
+    #data = data.sort_index(ascending=True);
+    data = data.sort_values(by=['NUMMER','JAHR'])
+    #data.groupby('NUMMER')
+    #data['INDIKATOR_WERT'].astype(float);
+    #data = data.groupby('NUMMER').apply(pd.DataFrame.sort, 'JAHR');
     data.to_csv(output);
-    print(data.groupby('NUMMER').mean().head());
+    print(data.head());
 
 if __name__ == "__main__":
     inp = sys.argv[1]
